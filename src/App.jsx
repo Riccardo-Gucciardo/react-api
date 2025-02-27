5050c26bf89aabb797cf0521f3777c25990436c1
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-27 09:30:29 +0100

    collegamento riuscito

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-
-import {useState,seEffect} from React
-import axios from "axios"
-
-function App() {
+import axios from "axios";
+import { useState,useEffect } from "react";
 
 
-  return (
-    <>
-     
-    </>
-  )
+export default function App() {
+// const [todos, setTodos] = useState([]);
+
+const [posts, setPosts] = useState([]);
+
+
+// Fetching dei dati
+// function fetchTodos() {
+// axios.get("https://jsonplaceholder.typicode.com/todos")
+// .then((res) => setTodos(res.data))
+// }
+
+function fetchPost() {
+  axios
+    .get("http://localhost:3000/api/posts")
+    .then((res) => setPosts(res.data))
 }
 
-export default App
+
+
+useEffect(fetchPost, [])
+
+
+return (
+    <div>
+      {/* <ul>
+        {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+))}
+      </ul> */}
+
+
+      <ul>
+        {posts.map((element) => (
+        <li key={element.id}>{element.title}</li>
+))}
+      </ul>
+     </div>
+);
+}
